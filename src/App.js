5e03a123acdc94fe94afbc7c46453fdd75ddb20f
Hieuhiphop <hieuhiphop797@gmail.com>
2022-07-31 22:55:02 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapComponent from './component/baitapcomponent/BaiTapComponent';
+import BaiTapThuKinh from './component/baitapthukinh/BaiTapThuKinh';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <BaiTapComponent/> */}
+     <BaiTapThuKinh/>
+    </div>
+  );
+}
+
+export default App;
+
